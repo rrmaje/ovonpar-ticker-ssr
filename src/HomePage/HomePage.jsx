00bf2cfeee0b3191409fa1e34f208ef629ab57f8
rrmaje <rafal.majewski@gmail.com>
2019-09-03 15:10:00 +0200

    Fix Home Page html

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -17,9 +17,9 @@ class HomePage extends React.Component {
         return (
             <div>
                 <p>You're logged in as {currentUser.username}</p>
-                <h2>Stock Ticker supports single session only in the current version. Market Reporting and Best Bid Offers are for the current session. Sessions start 8.00AM CET and end 4.00PM CET Monday through Friday.</h2>
-                <h2>Stock Ticker is not Trading Venue - it is not offering Financial Intruments under MiFID. Spot trading is facilitated by the service.</h2>
-                <h3>Thank you for using Ovonpar Stock Ticker</h3>
+                <p><strong>Stock Ticker supports single session only in the current version. Market Reporting and Best Bid Offers are for the current session. Sessions start 8.00AM CET and end 4.00PM CET Monday through Friday.</strong></p>
+                <p><strong>Stock Ticker is not Trading Venue - it is not offering Financial Intruments under MiFID. Spot trading is facilitated by the service.</strong></p>
+                <p><strong>Thank you for using Ovonpar Stock Ticker</strong></p>
             </div>
         );
     }
